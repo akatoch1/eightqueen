98c1077155ebd0e105a9f59c4e949e13ef3a0dd7
akatoch1 <44404876+akatoch1@users.noreply.github.com>
2019-09-16 17:17:45 -0400

    Add files via upload

--- EightQueen.cpp
+++ EightQueen.cpp
@@ -8,6 +8,7 @@
 #include "EightQueen.h"
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 EightQueen::EightQueen(int n){
@@ -21,23 +22,31 @@ EightQueen::EightQueen(int n){
 bool EightQueen::canBePlaced(vector<vector<int> > b, int row, int col) {
     int c1 = col;
     int c2 = col;
+
     for (unsigned int i = 0; i < col; i++) {
+
         if (b[row][i] == 1) {
+        	cout << "hey" << endl;
             return false;
         }
     }
     for (unsigned int i = row; i > 0; i--) {
+
         if (b[i][col] == 1) {
+        	cout << "heyy" << endl;
             return false;
         }
         c1 -= 1;
     }
     for (unsigned int i = row; i < num; i++) {
+
         if (b[i][col == 1]) {
+        	cout << "heyyy" << endl;
             return false;
         }
         c2 -= 1;
     }
+    cout << "heyyyy" << endl;
     return true;
 }
 
@@ -51,24 +60,25 @@ int EightQueen::queenPositions(vector<vector<int> > b, int col) {
     }
     for (int i = 0; i < num; i++) {
         if (canBePlaced(b, i, col)) {
-        	cout <<'true' << endl;
             b[i][col] = 1;
             col += 1;
             if (queenPositions(board, col) != 0) {
                 solution.push_back(queenPositions(b, col));
             }
+            b[i][col] = 0;
         }
     }
     return 0;
 }
 
 void EightQueen::printSolution() {
-    this -> queenPositions(board);
     if (solution.empty()) {
         cout << "No Solution!" << endl;
     }
+    else {
     for (int i = 0; i < solution.size(); i++) {
         cout << solution.at(i) << " ";
+    	}
     }
 }
 
